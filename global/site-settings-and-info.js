4501bd876940595b933742dabe752f32c758c302
40chalk <63959798+40chalk@users.noreply.github.com>
2022-03-24 17:38:29 -0500

    Update site-settings-and-info.js

--- global/site-settings-and-info.js
+++ global/site-settings-and-info.js
@@ -4,6 +4,13 @@ export const seriesOn = true
 // Turns contact form on. false = off true = on
 export const contactFormOn = false
 
+// Logo for site. You can choose between an image or text. If you choose text the alt text is what will render
+export const logo = {
+  imagePath: '/images/logo/logo.png',
+  alt: 'Example',
+  imageOrText: 'text',
+}
+
 // The text that shows up on home page
 export const aboutInfo =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -11,19 +18,6 @@ export const aboutInfo =
 // The text that shows up on contact page
 export const contactStatement =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad'
-
-// SEO meta tag data for each of the pages. What you write here will show up as the description in google.
-export const metaData = {
-  title: 'KrittIt Blog',
-  homePage:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.',
-  allPostsPage:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
-  seriesPage:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
-  contactPage:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
-}
 
 // Filters for posts. These should match the filterKeys in your post's md files
 export const filters = ['Lorem', 'ipsum', 'dolor', 'amet']
@@ -42,11 +36,17 @@ export const social = {
   youtube: '',
 }
 
-// Logo for site. You can choose between an image or text. If you choose text the alt text is what will render
-export const logo = {
-  imagePath: '/images/logo/logo.png',
-  alt: 'Example',
-  imageOrText: 'text',
+// SEO meta tag data for each of the pages. What you write here will show up as the description in google.
+export const metaData = {
+  title: 'KrittIt Blog',
+  homePage:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.',
+  allPostsPage:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  seriesPage:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
+  contactPage:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
 }
 
 // Paths for each page don't change!
